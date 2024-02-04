835780f3b1e36bd8fcc8be27ad4f35fd88125de3
Carlos Ceron <97488732+carlosarturoceron@users.noreply.github.com>
2024-02-03 23:09:00 -0600

    not working
    Call was rejected:
    Request ID: 1d5377fd0c39595ffce7ddb1f7571e0999d69670d663456ca5304173dce6039a
    Reject code: 5
    Reject text: Canister bkyz2-fmaaa-aaaaa-qaaaq-cai trapped explicitly: Uncaught TypeError: cannot read property 'fetch' of undefined at HTTPRequest (azle_main:71968)
    at http (azle_main:72110)
    at browserHTTPRequest (azle_main:72113)
    at loadLayersModel (azle_main:82892)
    at <anonymous> (azle_main:113218)
    at getResult (azle_main:61649)
    at executeMethod (azle_main:61623)
    at <anonymous> (azle_main:61688)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Canister, query, text, update, Void } from 'azle';
+import * as tf from '@tensorflow/tfjs';
 
 // This is a global variable used to stored the embeddings
 let message = '';
@@ -9,28 +10,23 @@ export default Canister({
         return message;
     }),
 
-    embed: update([text], Void, (newMessage) => {
-        // This is where the pretrained model needs to be implemented
-        // Need to solve issues with tf trying to use fetch to load model.
-        // A possible solution would be to use fs.promises.readFile function to read the contents of the file directly. It then parses the JSON and uses tf.io.fromMemory to create an IOHandler that loads the model from the parsed JSON object.
-        // Make sure to adjust the filePath variable with the correct local file path. Note that this approach is suitable for a Node.js environment; if you are working in a browser environment, you might need to use a different method to read the file content, such as using the File API or FileReader.
-        
-        // It would look something like this:
-        // // Load the TensorFlow.js model from the SavedModel
-        // const model = await tf.loadLayersModel('file:///Users/satoshi/Torah/1_Github/kool/hello_world_smallmodel/src/assets/archive/model.json'); // Note the three slashes after file: in the URL. This is the correct format for file URLs.
+    embed: update([text], Void, async (newMessage) => {
 
-        // // Prepare input data
-        // const sentences = newMessage.split(','); // Assuming newMessage is a comma-separated string
-        // const textEncoder = new TextEncoder();
-        // const inputTensor = tf.tensor(sentences.map(sentence => textEncoder.encode(sentence)));
+        // Load the TensorFlow.js model from the SavedModel
+        const model = await tf.loadLayersModel('file:///Users/satoshi/Torah/1_Github/kool/hello_world_smallmodel/src/assets/archive/model.json'); // Note the three slashes after file: in the URL. This is the correct format for file URLs.
 
-        // // Make predictions using the loaded model
-        // const embeddings = model.predict(inputTensor);
+        // Prepare input data
+        const sentences = newMessage.split(','); // Assuming newMessage is a comma-separated string
+        const textEncoder = new TextEncoder();
+        const inputTensor = tf.tensor(sentences.map(sentence => textEncoder.encode(sentence)));
 
-        // // Convert the embeddings to a string or any other desired format
-        // message = embeddings.toString();
+        // Make predictions using the loaded model
+        const embeddings = model.predict(inputTensor);
 
-        message = "[0.1,-0.1,-0.2]"; // we transform documents in to vectorspaces so that we can create a multi dimentional vector space in which vectors close together have semantic similarity!
+        // Convert the embeddings to a string or any other desired format
+        message = embeddings.toString();
+
+        // message = "[0.1,-0.1,-0.2]"; // we transform documents in to vectorspaces so that we can create a multi dimentional vector space in which vectors close together have semantic similarity!
         // then we can query that vector DB to do Retrieval Augmented Generation workloads
         // Having decentralized endpoints to run AI services is a crucial step forward in the AI lifecycle
         // mainly because of the censorship ressitance
